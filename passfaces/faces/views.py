233aff8d36978393d4f38138e116db14f9324098
emilyyoungss <emily@DESKTOP-GFAQJR7.localdomain>
2019-01-03 17:47:36 +0000

    Basic django application

---
+++ passfaces/faces/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+def index(request):
+        context = RequestContext(request)
+        context_dict = {'boldmessage': "I am bold font from the context"}
+        return render_to_response('faces/index.html', context_dict, context)
