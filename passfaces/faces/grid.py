d041d38de679f719d0906c1372333fa12ee441e5
emilyyoungss <emilyyoungss@hotmail.co.uk>
2019-01-08 17:50:44 +0000

    attempt to put image in grid
    Implementing grid using Tkinter/Pillow for python file
    
    For grid.html attempting to use html and JS to make image grid instead

--- passfaces/faces/grid.py
+++ passfaces/faces/grid.py
@@ -1,12 +1,57 @@
-import numpy as np
-import cv2
-from matplotlib import pyplot as plt
+# import Image
+#
+# #opens an image:
+# im = Image.open("old_man.jpg")
+#
+# #creates a new empty image, RGB mode, and size 400 by 400.
+# new_im = Image.new('RGB', (400,400))
+#
+# #Here I resize my opened image, so it is no bigger than 100,100
+# im.thumbnail((100,100))
+# #Iterate through a 4 by 4 grid with 100 spacing, to place my image
+# for i in xrange(0,400,100):
+#     for j in xrange(0,400,100):
+#         #I change brightness of the images, just to emphasise they are unique copies.
+#         im=Image.eval(im,lambda x: x+(i+j)/30)
+#         #paste the image at location i,j:
+#         new_im.paste(im, (i,j))
+#
+# new_im.show()
 
-# Load an color image in colour
-img = cv2.imread('old_man.jpg',1)
 
-cv2.imshow('old-man',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+import Tkinter as tk
+from PIL import Image, ImageTk
+# position coordinates of root 'upper left corner'
+x = 0
+y = 0
+
+root = tk.Tk()
+root.title('passfaces')
+# make the root window the size of the image
+root.geometry("%dx%d+%d+%d" % (w, h, x, y))
+
+# pick an image file you have .bmp  .jpg  .gif.  .png
+# load the file and covert it to a Tkinter image object
+oldman = "old_man.jpg"
+oldman = ImageTk.PhotoImage(Image.open(imageFile))
 
 
+
+# get the image size
+w = oldman.width()
+h = oldman.height()
+
+
+# root has no image argument, so use a label as a panel
+panel1 = tk.Label(root, image=oldman)
+panel1.pack(side='top', fill='both', expand='yes')
+
+# put a button on the image panel to test it
+button2 = tk.Button(panel1, text='button2')
+button2.pack(side='top')
+
+# save the panel's image from 'garbage collection'
+panel1.image = oldman
+
+# start the event loop
+root.mainloop()
